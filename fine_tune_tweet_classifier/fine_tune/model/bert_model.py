softmax bugfix

fine_tune_tweet_classifier/fine_tune/model/bert_model.py
@@ -2,8 +2,9 @@ import transformers
 import torch.nn
 
 class BERTModel(torch.nn.Module):
-    def __init__(self, bert: transformers.BertModel, layer_infos: list[dict]):
+    def __init__(self, bert: transformers.BertModel, layer_infos: list[dict], num_classes:int):
         super(BERTModel, self).__init__()
+        self.num_classes = num_classes
 
         self.bert = bert
         
@@ -20,7 +21,7 @@ class BERTModel(torch.nn.Module):
             layer = torch.nn.Dropout(layer_info["p"])
 
         elif layer_info["name"] == "softmax":
-            layer = torch.nn.Softmax()
+            layer = torch.nn.Softmax(dim=self.num_classes)
         elif layer_info["name"] == "relu":
             layer = torch.nn.ReLU()
 

fine_tune_tweet_classifier/fine_tune/trainer.py
@@ -22,7 +22,7 @@ class Trainer():
         self.device = torch.device(self.config_service.device)
 
         bert = transformers.BertModel.from_pretrained(self.config_service.bert_model_name)
-        self.model = BERTModel(bert, self.config_service.layers).to(self.device)
+        self.model = BERTModel(bert, self.config_service.layers, self.config_service.num_classes).to(self.device)
 
     def test(self):
         labeled_tweets = self.data_service.read_test_tweets()